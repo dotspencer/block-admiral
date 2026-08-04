50a832d4aeb815a08e292a7ff0a1e632ed09da1b
Spencer Smith <dontaskspencer@gmail.com>
2026-08-04 12:57:13 -0600

    Add adblock format

--- scripts/format.js
+++ scripts/format.js
@@ -7,8 +7,14 @@ const set = new Set(lines);
 const list = Array.from(set);
 list.sort();
 
+// create hosts
 const hostsOutput = list.map((domain) => `0.0.0.0     ${domain}`).join('\n');
 fs.writeFileSync('./hosts', hostsOutput);
 
+// create adblock
+const adblockOutput = list.map((domain) => `||${domain}^`).join('\n');
+fs.writeFileSync('./adblock.txt', adblockOutput);
+
+// sorted list
 const listOutput = list.join('\n');
 fs.writeFileSync('./list.txt', listOutput);
